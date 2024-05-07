5e0bcdacab54f82ca2f622bf180e4406183d2a46
Andréia <andreia.martins17@escola.pr.gov.br>
2024-05-06 21:14:59 -0300

    Ajuste de datas

--- main.js
+++ main.js
@@ -16,10 +16,10 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 
-const tempoObjetivo1 = new Date("2024-10-05T23:59");
-const tempoObjetivo2 = new Date("2024-10-05T23:59");
-const tempoObjetivo3 = new Date("2024-10-05T23:59");
-const tempoObjetivo4 = new Date("2024-10-05T23:59");
+const tempoObjetivo1 = new Date("2024-05-06T23:59");
+const tempoObjetivo2 = new Date("2024-05-29T23:59");
+const tempoObjetivo3 = new Date("2024-07-10T23:59");
+const tempoObjetivo4 = new Date("2024-12-01T23:59");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
